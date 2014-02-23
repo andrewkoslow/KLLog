7ef42df5aac6cff143da52921ac653645040751f
Andrew Koslow <andrewkoslow@gmail.com>
2014-02-23 21:03:08 +0400

    Added macro for logging message-variable pairs

--- KLLog/KLLog.h
+++ KLLog/KLLog.h
@@ -36,6 +36,7 @@
 #define KLLog(variables...)         KLInform(variables)
 #define KLLogM(message)             KLInformM(message)
 #define KLLogV(variables...)        KLInformV(variables)
+#define KLLogP(pairs...)            KLInformP(pairs)
 #define KLLogS()                    KLInformS()
 #define KLLogSM(message)            KLInformSM(message)
 #define KLLogSV(variables...)       KLInformSV(variables)
@@ -43,6 +44,7 @@
 #define KLInform(variables...)      KLInformSV(variables)
 #define KLInformM(message)          KLLogMes(KL_LOG_ENABLE, KL_LOG_LEVEL_DEBUG, message)
 #define KLInformV(variables...)     KLLogVars(KL_LOG_ENABLE, KL_LOG_LEVEL_DEBUG, variables)
+#define KLInformP(pairs...)         KLLogPairs(KL_LOG_ENABLE, KL_LOG_LEVEL_DEBUG, pairs)
 #define KLInformS()                 KLLogSel(KL_LOG_ENABLE, KL_LOG_LEVEL_DEBUG)
 #define KLInformSM(message)         KLLogSelMes(KL_LOG_ENABLE, KL_LOG_LEVEL_DEBUG, message)
 #define KLInformSV(variables...)    KLLogSelVars(KL_LOG_ENABLE, KL_LOG_LEVEL_DEBUG, variables)
@@ -50,6 +52,7 @@
 #define KLWarn(variables...)        KLWarnSV(variables)
 #define KLWarnM(message)            KLLogMes(KL_LOG_ENABLE, KL_LOG_LEVEL_WARN, message)
 #define KLWarnV(variables...)       KLLogVars(KL_LOG_ENABLE, KL_LOG_LEVEL_WARN, variables)
+#define KLWarnP(pairs...)           KLLogPairs(KL_LOG_ENABLE, KL_LOG_LEVEL_WARN, pairs)
 #define KLWarnS()                   KLLogSel(KL_LOG_ENABLE, KL_LOG_LEVEL_WARN)
 #define KLWarnSM(message)           KLLogSelMes(KL_LOG_ENABLE, KL_LOG_LEVEL_WARN, message)
 #define KLWarnSV(variables...)      KLLogSelVars(KL_LOG_ENABLE, KL_LOG_LEVEL_WARN, variables)
@@ -57,6 +60,7 @@
 #define KLPanic(variables...)       KLPanicSV(variables)
 #define KLPanicM(message)           KLLogMes(KL_LOG_ENABLE, KL_LOG_LEVEL_EMERG, message)
 #define KLPanicV(variables...)      KLLogVars(KL_LOG_ENABLE, KL_LOG_LEVEL_EMERG, variables)
+#define KLPanicP(pairs...)          KLLogPairs(KL_LOG_ENABLE, KL_LOG_LEVEL_EMERG, pairs)
 #define KLPanicS()                  KLLogSel(KL_LOG_ENABLE, KL_LOG_LEVEL_EMERG)
 #define KLPanicSM(message)          KLLogSelMes(KL_LOG_ENABLE, KL_LOG_LEVEL_EMERG, message)
 #define KLPanicSV(variables...)     KLLogSelVars(KL_LOG_ENABLE, KL_LOG_LEVEL_EMERG, variables)
@@ -66,6 +70,7 @@
 
 #define KLLogMes(boundary, level, message) ____KLLogBoundaryWrapper(boundary, ____KLLog, level, message)
 #define KLLogVars(boundary, level, variables...) ____KLLogBoundaryWrapper(boundary, ____KLLogVars, level, ____KLLogArgCount(variables), variables)
+#define KLLogPairs(boundary, level, pairs...) ____KLLogBoundaryWrapper(boundary, ____KLLogPairs, level, ____KLLogArgCount(pairs), pairs)
 #define KLLogSel(boundary, level) ____KLLogBoundaryWrapper(boundary, ____KLLogSel, level)
 #define KLLogSelMes(boundary, level, message) ____KLLogBoundaryWrapper(boundary, ____KLLogSelMes, level, message)
 #define KLLogSelVars(boundary, level, variables...) ____KLLogBoundaryWrapper(boundary, ____KLLogSelVars, level, ____KLLogArgCount(variables), variables)
@@ -75,6 +80,10 @@
 
 #define ____KLLogVars(____level, ARG_COUNT, ...) {\
     ____KLLog(____level, ([[NSArray arrayWithObjects:____KLLogVal ## ARG_COUNT(__VA_ARGS__), nil] componentsJoinedByString:@", "]));\
+}
+
+#define ____KLLogPairs(____level, ARG_COUNT, ...) {\
+____KLLog(____level, ([[NSArray arrayWithObjects:____KLLogPair ## ARG_COUNT(__VA_ARGS__), nil] componentsJoinedByString:@""]));\
 }
 
 #define ____KLLogSel(____level) {\
@@ -105,14 +114,19 @@
 #define ____KLLogArgCount(...) ____KLLogArgCountImpl(__VA_ARGS__, 8, 7, 6, 5, 4, 3, 2, 1)
 #define ____KLLogArgCountImpl(_1,_2,_3,_4,_5,_6,_7,_8,N,...) N
 
-#define ____KLLogVal8(v1, ...) ____KLLogVal(v1), ____KLLogVal7(__VA_ARGS__)
-#define ____KLLogVal7(v1, ...) ____KLLogVal(v1), ____KLLogVal6(__VA_ARGS__)
-#define ____KLLogVal6(v1, ...) ____KLLogVal(v1), ____KLLogVal5(__VA_ARGS__)
-#define ____KLLogVal5(v1, ...) ____KLLogVal(v1), ____KLLogVal4(__VA_ARGS__)
-#define ____KLLogVal4(v1, ...) ____KLLogVal(v1), ____KLLogVal3(__VA_ARGS__)
-#define ____KLLogVal3(v1, ...) ____KLLogVal(v1), ____KLLogVal2(__VA_ARGS__)
-#define ____KLLogVal2(v1, ...) ____KLLogVal(v1), ____KLLogVal1(__VA_ARGS__)
-#define ____KLLogVal1(v1) ____KLLogVal(v1)
+#define ____KLLogVal8(v, ...) ____KLLogVal(v), ____KLLogVal7(__VA_ARGS__)
+#define ____KLLogVal7(v, ...) ____KLLogVal(v), ____KLLogVal6(__VA_ARGS__)
+#define ____KLLogVal6(v, ...) ____KLLogVal(v), ____KLLogVal5(__VA_ARGS__)
+#define ____KLLogVal5(v, ...) ____KLLogVal(v), ____KLLogVal4(__VA_ARGS__)
+#define ____KLLogVal4(v, ...) ____KLLogVal(v), ____KLLogVal3(__VA_ARGS__)
+#define ____KLLogVal3(v, ...) ____KLLogVal(v), ____KLLogVal2(__VA_ARGS__)
+#define ____KLLogVal2(v, ...) ____KLLogVal(v), ____KLLogVal1(__VA_ARGS__)
+#define ____KLLogVal1(v) ____KLLogVal(v)
+
+#define ____KLLogPair8(m, v, ...) ____KLLogPair2(m, v), ____KLLogPair6(__VA_ARGS__)
+#define ____KLLogPair6(m, v, ...) ____KLLogPair2(m, v), ____KLLogPair4(__VA_ARGS__)
+#define ____KLLogPair4(m, v, ...) ____KLLogPair2(m, v), ____KLLogPair2(__VA_ARGS__)
+#define ____KLLogPair2(m, v) m, ____KLLogVal(v)
 
 #define ____KLLogExprByType(v, t, e1, e2) __builtin_choose_expr(__builtin_types_compatible_p(__typeof(v), t), e1, e2)
 #define ____KLLogExprByTypeEval(...) ____KLLogExprByType(__VA_ARGS__)
